8df700d0d2230de0a0f64b5afb4c16c982587062
jaeik <wodlr2007@naver.com>
2021-02-24 15:01:22 +0900

    [백준] 이분 탐색 - 2470. 두 용액 ( 골드 V )
    - 이분 탐색
    - 무난한 난이도

---
+++ BaekJoon/이진 탐색/2470.twoLiquid.js
@@ -0,0 +1,43 @@
+const rl = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = [];
+
+rl.on("line", (ln) => {
+  input.push(ln);
+}).on("close", () => {
+  const N = Number(input.shift());
+  const list = input
+    .shift()
+    .split(" ")
+    .map((n) => n - 0)
+    .sort((a, b) => a - b);
+
+  let [left, right] = [0, N - 1];
+  let min = Math.abs(list[left] + list[right]);
+  let ans = [list[left], list[right]];
+
+  while (left < right) {
+    const sum = list[left] + list[right];
+    console.log(left, right, sum, min);
+    if (sum === 0) {
+      ans = [list[left], list[right]];
+      break;
+    }
+    if (min > Math.abs(sum)) {
+      ans = [list[left], list[right]];
+      min = Math.abs(sum);
+    }
+    if (sum > 0) {
+      right--;
+      continue;
+    }
+    if (sum < 0) {
+      left++;
+      continue;
+    }
+  }
+  console.log(ans[0] + " " + ans[1]);
+});
